3cc9b9c517bf37b05421a617939f61173e544b70
Hemanth Mudra <47498538+hmswrth@users.noreply.github.com>
2020-03-11 14:42:03 +0530

    commit

---
+++ src/ReadSingleCell.java
@@ -0,0 +1,20 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadSingleCell {
+
+	public static void main(String[] args) throws IOException {
+		FileInputStream fis = new FileInputStream("sample_test.xlsx");
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet ws = wb.getSheetAt(0);
+		String str = String.valueOf(ws.getRow(0).getCell(0));
+		String str1 = String.valueOf(ws.getRow(1).getCell(0));
+		wb.close();
+		System.out.println(str +" "+ str1);
+	}
+
+}
